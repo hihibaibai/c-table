ea6547e145d9ae8b7893e7567d3dc028fa17e120
wilson <979597463@qq.com>
2023-12-05 19:51:53 +0800

    init

---
+++ src/view/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div id="tablePlace"></div>
+  </div>
+</template>
+
+<script>
+import table from '@/components/table/index'
+export default {
+  name: "index",
+  mounted() {
+    this.xs = table.create('#tablePlace',()=>1200,()=>400).render();
+  },
+  data(){
+    return {
+      xs:{}
+    }
+  },
+}
+</script>
+
+
+
+<style scoped>
+
+</style>

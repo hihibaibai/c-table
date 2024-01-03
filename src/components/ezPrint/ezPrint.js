91d01fd20a85c5c8e0e8bc53e01bf7a65ee30092
wilson <979597463@qq.com>
2024-01-03 16:58:23 +0800

    add setPrintContent function

--- src/components/ezPrint/ezPrint.js
+++ src/components/ezPrint/ezPrint.js
@@ -1,35 +1,108 @@
+import {col} from "@/components/table/data";
+
 export default class ezPrint{
   constructor() {
     this.iframe = {};
-    this.document = {};
+    this.iframeDocument = {};
     this.body = {};
+    this.loadNumber = 0;
+    this.loadImage = [];
+    this.readyForPrint = false;
     this.iframe = document.createElement('iframe');
     let iframe = this.iframe;
     // iframe.setAttribute('style','position:absolute;width:0;height:0;left:-5000px;top:-5000px')
-    iframe.setAttribute('style','position:absolute')
+    iframe.setAttribute('style','position:absolute;width:794px;height:1123px')
 
     document.getElementsByTagName('body')[0].appendChild(this.iframe);
-    // iframe.contentWindow.document.body.appendChild(iframe.contentDocument.createElement('p'))
-    // this.ifrDocument = this.iframe.contentDocument;
-    iframe.onload = ()=>{
-      this.document = iframe.contentDocument;
-      this.document.firstChild.width;
-      console.log(this.document);
-      let document = this.document;
-      let bodyTag = this.document.body;
+
+    if (navigator.userAgent.includes("Firefox")){
+      this.iframe.contentWindow.onload = () =>{
+        // iframe.onload = ()=>{
+        this.iframeDocument = iframe.contentDocument;
+        this.iframeDocument.firstChild.width;
+        console.log(this.iframeDocument);
+        let document = this.iframeDocument;
+        let bodyTag = this.iframeDocument.body;
+        this.body = bodyTag;
+        let headTag = this.iframeDocument.head;
+        this.headTag = headTag;
+
+        let PPI = getPPI();
+        // debugger
+        let pageWidthInPx = Math.round(210*(PPI/25.4));
+        let pageHeightInPx = Math.round(297*(PPI/25.4));
+        document.firstChild.style.width=pageWidthInPx;
+        document.firstChild.style.height=pageHeightInPx-1;
+
+        let style = document.createElement('style');
+        style.innerText=
+            'body {' +
+            ` width:${pageWidthInPx}px;`+
+            ` height:${pageHeightInPx-1}px;`+
+            ' margin: 0;' +
+            '}' +
+            'table {' +
+            ` width:${pageWidthInPx}px;`+
+            ` height:${pageHeightInPx-1}px;`+
+            // ' background-color: black;'+
+            '}' +
+            'table.content-container {' +
+            '    page-break-after:always;' +
+            '}' +
+            'thead.content-header {' +
+            '    display:table-header-group;' +
+            '}' +
+            'tfoot.content-footer {' +
+            ' position: absolute;' +
+            ' bottom: 0;' +
+            ' right: 0;' +
+            ' display:table-footer-group;' +
+            '}' +
+            '@page {' +
+            ' size:A4;' +
+            ' margin:0' +
+            '}'
+        headTag.appendChild(style);
+      }
+    }
+    else {
+      this.iframeDocument = iframe.contentDocument;
+      this.iframeDocument.firstChild.width;
+      console.log(this.iframeDocument);
+      let bodyTag = this.iframeDocument.body;
       this.body = bodyTag;
-      let headTag = this.document.head;
+      let headTag = this.iframeDocument.head;
       this.headTag = headTag;
-      let style = document.createElement('style');
+      let PPI = getPPI();
+      // debugger
+      let pageWidthInPx = Math.round(210*(PPI/25.4));
+      let pageHeightInPx = Math.round(297*(PPI/25.4));
+      this.iframeDocument.firstChild.style.width=pageWidthInPx;
+      this.iframeDocument.firstChild.style.height=pageHeightInPx-1;
+
+      let style = this.iframeDocument.createElement('style');
       style.innerText=
-          'table.content-container {\n' +
-          '    page-break-after:always;\n' +
-          '}\n' +
-          'thead.content-header {\n' +
-          '    display:table-header-group;\n' +
-          '}\n' +
-          'tfoot.content-footer {\n' +
-          '    display:table-footer-group;\n' +
+          'body {' +
+          ` width:${pageWidthInPx}px;`+
+          ` height:${pageHeightInPx-1}px;`+
+          ' margin: 0;' +
+          '}' +
+          'table {' +
+          ` width:${pageWidthInPx}px;`+
+          ` height:${pageHeightInPx-1}px;`+
+          // ' background-color: black;'+
+          '}' +
+          'table.content-container {' +
+          '    page-break-after:always;' +
+          '}' +
+          'thead.content-header {' +
+          '    display:table-header-group;' +
+          '}' +
+          'tfoot.content-footer {' +
+          ' position: absolute;' +
+          ' bottom: 0;' +
+          ' right: 0;' +
+          ' display:table-footer-group;' +
           '}' +
           '@page {' +
           ' size:A4;' +
@@ -37,57 +110,86 @@ export default class ezPrint{
           '}'
       headTag.appendChild(style);
     }
-    console.log(iframe);
+
+    this.iframe.contentWindow.onafterprint = (event) =>{
+      console.log(event)
+      document.getElementsByTagName('body')[0].removeChild(this.iframe);
+    }
     return this;
   }
 
-  setPrintContent(){
-    // console.log(this.ifrDocument);
-    // let ifrDocument = this.ifrDocument;
-    let tableTag = this.iframe.contentDocument.createElement('table');
-    let theadTag = this.iframe.contentDocument.createElement('thead');
-    let tfootTag = this.iframe.contentDocument.createElement('tfoot');
-    let tbodyTag = this.iframe.contentDocument.createElement('tbody');
-    //
+  /**
+   * 下面这个函数会把传入的变量塞到tbody里面去。
+   * 允许传入的是base64的字符串，或者Image对象
+   */
+  setPrintContent(content){
+    let tableTag = this.iframeDocument.createElement('table');
+    let theadTag = this.iframeDocument.createElement('thead');
+    let tfootTag = this.iframeDocument.createElement('tfoot');
+    let tbodyTag = this.iframeDocument.createElement('tbody');
+
     theadTag.classList.add("content-header");
     tfootTag.classList.add("content-footer");
     tableTag.classList.add("content-container");
-    // theadTag.setAttribute('display', 'table-header-group');
-    // tfootTag.setAttribute('display', 'table-footer-group');
-    // tableTag.setAttribute('page-break-after', 'always');
 
-    let headContent = this.document.createElement('div');
-    headContent.innerText = '这个是表头的内容';
-    theadTag.appendChild(this.document.createElement('tr').appendChild(this.document.createElement('th').appendChild(headContent)))
-    //
+    let headContent = this.iframeDocument.createElement('div');
+    // headContent.innerText = '这个是表头的内容';
+    let bodyContent = this.iframeDocument.createElement('div');
+    if (content instanceof Image){
+      this.loadImage.push(content.src);
+      content.onload = ()=>{
+        this.loadCompleteCallback();
+      }
+      bodyContent.appendChild(content);
+    }
+    if ( typeof content === "string"){
+      let imageObj = new Image();
+      imageObj.src=content;
+      this.loadImage.push(content);
+      imageObj.onload = () =>{
+        this.loadCompleteCallback();
+      }
+      bodyContent.appendChild(imageObj);
+    }
+
+    let footContent = this.iframeDocument.createElement('div');
+    // footContent.innerText = '这个是表尾内容';
+    theadTag.appendChild(this.iframeDocument.createElement('tr').appendChild(this.iframeDocument.createElement('th').appendChild(headContent)))
+    tfootTag.appendChild(this.iframeDocument.createElement('tr').appendChild(this.iframeDocument.createElement('td').appendChild(footContent)))
+    tbodyTag.appendChild(this.iframeDocument.createElement('tr').appendChild(this.iframeDocument.createElement('td').appendChild(bodyContent)))
+
     tableTag.appendChild(theadTag);
     tableTag.appendChild(tfootTag);
     tableTag.appendChild(tbodyTag);
-    //
-    // let a = this.iframe.contentDocument.createElement('p')
-    // a.innerText='111';
-    this.body.appendChild(tableTag)
-    // console.log(this.iframe);
-    // this.iframe.contentDocument.appendChild(tableTag);
-    console.log(getPPI());
 
+    this.body.appendChild(tableTag)
   }
 
   print(){
-    this.iframe.contentWindow.print();
+    if (this.readyForPrint){
+      this.iframe.contentWindow.print();
+    }
   }
+
+  loadCompleteCallback(){
+    this.loadNumber ++;
+    if (this.loadNumber == this.loadImage.length){
+      this.readyForPrint = true;
+    }
+  }
+
 }
 
 function getPPI(){
   // create an empty element
-  var div = document.createElement('div');
+  let div = document.createElement('div');
 // give it an absolute size of one inch
   div.style.width='1in';
 // append it to the body
-  var body = document.getElementsByTagName('body')[0];
+  let body = document.getElementsByTagName('body')[0];
   body.appendChild(div);
 // read the computed width
-  var ppi = document.defaultView.getComputedStyle(div, null).getPropertyValue('width');
+  let ppi = document.defaultView.getComputedStyle(div, null).getPropertyValue('width');
 // remove it again
   body.removeChild(div);
 // and return the value

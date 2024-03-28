766440422e6205479ada5b8d4fcf397f79924f4b
wilson <979597463@qq.com>
2024-03-28 16:51:42 +0800

    add sample data for test

---
+++ src/view/testJSON.js
@@ -0,0 +1,319 @@
+let a = {
+  "rows": {
+    "len": 100
+  },
+  "cols": {
+    "1": {
+      "width": 65
+    },
+    "2": {
+      "width": 88
+    },
+    "len": 26
+  },
+  "rowHeight": 25,
+  "colWidth": 100,
+  "scroll": [
+    0,
+    0,
+    0,
+    0
+  ],
+  "style": {
+    "bgcolor": "#FFF",
+    "color": "#333",
+    "align": "left",
+    "valign": "middle",
+    "textwrap": false,
+    "bold": false,
+    "italic": false,
+    "fontFamily": "Roboto",
+    "fontSize": 10,
+    "underline": false,
+    "strikethrough": false,
+    "border": {}
+  },
+  "styles": [
+    {
+      "bgcolor": "#FFF",
+      "color": "#333",
+      "align": "left",
+      "valign": "middle",
+      "textwrap": false,
+      "bold": false,
+      "italic": false,
+      "fontFamily": "Roboto",
+      "fontSize": 10,
+      "underline": false,
+      "strikethrough": false,
+      "border": {}
+    },
+    {
+      "bgcolor": "#FFF",
+      "color": "#333",
+      "align": "left",
+      "valign": "middle",
+      "textwrap": true,
+      "bold": false,
+      "italic": false,
+      "fontFamily": "Roboto",
+      "fontSize": 10,
+      "underline": false,
+      "strikethrough": false,
+      "border": {}
+    }
+  ],
+  "borders": [
+    {
+      "top": [
+        "thin",
+        "#000"
+      ],
+      "left": [
+        "thin",
+        "#000"
+      ],
+      "right": [
+        "thin",
+        "#000"
+      ],
+      "bottom": [
+        "thin",
+        "#000"
+      ]
+    }
+  ],
+  "merges": [
+    "C1:F1",
+    "G1:I1",
+    "B1:B2",
+    "A1:A2"
+  ],
+  "cells": [
+    [
+      0,
+      1,
+      {
+        "style": 0,
+        "value": "预警信息",
+        "border": 0
+      }
+    ],
+    [
+      0,
+      2,
+      {
+        "style": 0,
+        "border": 0,
+        "value": "客户信息"
+      }
+    ],
+    [
+      0,
+      0,
+      {
+        "style": 0,
+        "value": "序号",
+        "border": 0
+      }
+    ],
+    [
+      1,
+      1,
+      {
+        "style": 0,
+        "border": 0
+      }
+    ],
+    [
+      1,
+      2,
+      {
+        "style": 0,
+        "value": "首营审批号",
+        "border": 0
+      }
+    ],
+    [
+      1,
+      3,
+      {
+        "value": "客户编号",
+        "style": 0,
+        "border": 0
+      }
+    ],
+    [
+      1,
+      4,
+      {
+        "style": 0,
+        "border": 0,
+        "value": "客户名称"
+      }
+    ],
+    [
+      1,
+      5,
+      {
+        "style": 0,
+        "border": 0,
+        "value": "企业分类"
+      }
+    ],
+    [
+      1,
+      6,
+      {
+        "style": 0,
+        "value": "证件名称",
+        "border": 0
+      }
+    ],
+    [
+      1,
+      7,
+      {
+        "value": "证件编号",
+        "style": 0,
+        "border": 0
+      }
+    ],
+    [
+      0,
+      5,
+      {
+        "style": 0,
+        "border": 0
+      }
+    ],
+    [
+      0,
+      3,
+      {
+        "value": "",
+        "border": 0
+      }
+    ],
+    [
+      0,
+      4,
+      {
+        "value": "",
+        "border": 0
+      }
+    ],
+    [
+      0,
+      6,
+      {
+        "value": "证件有效期",
+        "border": 0,
+        "style": 0
+      }
+    ],
+    [
+      0,
+      7,
+      {
+        "value": "",
+        "border": 0
+      }
+    ],
+    [
+      1,
+      0,
+      {
+        "value": "",
+        "border": 0
+      }
+    ],
+    [
+      2,
+      0,
+      {
+        "value": "#{数据集.序号}",
+        "border": 0
+      }
+    ],
+    [
+      2,
+      1,
+      {
+        "value": "#{数据集.预警信息}",
+        "border": 0
+      }
+    ],
+    [
+      2,
+      2,
+      {
+        "value": "#{数据集.首营审批号}",
+        "border": 0
+      }
+    ],
+    [
+      2,
+      3,
+      {
+        "value": "#{数据集.客户编号}",
+        "border": 0,
+        "style": 0
+      }
+    ],
+    [
+      2,
+      4,
+      {
+        "value": "#{数据集.客户名称}",
+        "border": 0,
+        "style": 1
+      }
+    ],
+    [
+      2,
+      5,
+      {
+        "value": "#{数据集.企业分类}",
+        "border": 0,
+        "style": 1
+      }
+    ],
+    [
+      2,
+      6,
+      {
+        "value": "#{数据集.证件名称}",
+        "border": 0,
+        "style": 1
+      }
+    ],
+    [
+      2,
+      7,
+      {
+        "value": "#{数据集.证件编号}",
+        "border": 0,
+        "style": 1
+      }
+    ],
+    [
+      1,
+      8,
+      {
+        "style": 0,
+        "value": "证件有效期",
+        "border": 0
+      }
+    ],
+    [
+      2,
+      8,
+      {
+        "value": "#{数据集.证件有效期}",
+        "border": 0
+      }
+    ]
+  ]
+}
+

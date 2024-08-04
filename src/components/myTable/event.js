634938009e500ee44228008448cf20a7819435c4
wilson <979597463@qq.com>
2024-08-04 21:19:06 +0800

    add select effect

--- src/components/myTable/event.js
+++ src/components/myTable/event.js
@@ -1,14 +1,18 @@
+import Cells from '@/components/myTable/cells';
+
 export function eventInit(table) {
   let {canvas} = table;// 这里能在canvas配置点击事件的要求是元素前方的元素有pointerEvents属性为none才行
   canvas.addEventListener('mousedown',
       (event) => handleMousedown(table, event));
 }
 
-function handleMousedown(table, event) {
+function handleMousedown(table, event) {// 这个函数处理单元格的选择
   console.log(event.offsetX,event.offsetY);
   console.log(event.target);
   let cellPosition = table.scrollBar.getCellIndexByXYOffset(event.offsetX,
       event.offsetY);
   console.log(cellPosition);
+  table.overLayer.selectCells(cellPosition);
+
   event.stopPropagation();
 }
--- src/components/myTable/overLayer.js
+++ src/components/myTable/overLayer.js
@@ -30,10 +30,16 @@ const defaultData = { // 只是为了方便开发使用的
 };
 
 export default class OverLayer {
+  data;
+  viewport;
+  selectedCell;// 这里放选中的那个单元格的元素
   headOverLayerElement = [];
   bodyOverLayerElement = []; // 这里的overLayer元素按照象限来划分，第一个是右上角的冻结部分，接着是左上角的冻结，最后是右下角部分是数据的部分
 
-  constructor(container,data,width,height) {
+  constructor(container,table,width,height) {
+    let {data, scrollBar} = table;
+    this.data = data;
+    this.viewport = scrollBar.getViewport();
     let {freeze} = defaultData;
 
     let element = {};
@@ -78,5 +84,79 @@ export default class OverLayer {
     let element = document.createElement('div');
     ElementOperator.setClass(element, `${stylePrefix}--OverLayer--body`);
     return element;
+  }
+
+  selectCells(cellPosition) { // 暂时只处理单个选择
+    let cell;
+    if (cellPosition.placement === 'body') {
+      cell = Cells.getCell(this.data, cellPosition.x, cellPosition.y);
+      console.log(cell);
+      this.getXYWidthHeightByCell(cellPosition);
+    }
+
+
+  }
+
+  getXYWidthHeightByCell(cellPosition){
+    let element = this.bodyOverLayerElement[3];
+    let overLayerWidth = parseInt(window.getComputedStyle(element).getPropertyValue('width'));
+    let overLayerHeight = parseInt(window.getComputedStyle(element).getPropertyValue('height'));
+    let leftValue = 0;
+    let topValue = 0;
+    let widthValue = 0;
+    let heightValue = 0;
+    let widthCount = 0;
+    let heightCount = 0;
+    let x = this.viewport[0];
+    let y = this.viewport[1];
+    const startX = this.viewport[0];
+    const startY = this.viewport[1];
+    while (overLayerWidth - widthCount >= 0) {
+      let cellWidth = 0;
+      if (this.data.cols[x]) {
+        cellWidth = this.data.cols[x];
+      }
+      else {
+        cellWidth = this.data.colWidth;
+      }
+      if (x === cellPosition.x) {
+        leftValue = widthCount;
+        widthValue = cellWidth;
+      }
+      widthCount = widthCount + cellWidth;
+      x++;
+    }
+    while (overLayerHeight - heightCount >= 0) {
+      let cellHeight = 0;
+      if (this.data.rows[y]) {
+        cellHeight = this.data.rows[y];
+      }
+      else {
+        cellHeight = this.data.rowHeight;
+      }
+      if (y === cellPosition.y) {
+        topValue = heightCount;
+        heightValue = cellHeight;
+      }
+      heightCount = heightCount + cellHeight;
+      y++;
+    }
+    console.log(leftValue, topValue, widthValue, heightValue);
+    let overLayer = this.selectedCell;
+    if (overLayer != null) {
+      overLayer.remove();
+    }
+    overLayer = document.createElement('div');
+    ElementOperator.setWidth(overLayer, widthValue-2);// 这里的magic number 是为了渲染边框的时候位置正确
+    ElementOperator.setHeight(overLayer, heightValue-2);
+    ElementOperator.setLeft(overLayer, leftValue-1);
+    ElementOperator.setTop(overLayer, topValue-1);
+    ElementOperator.setPosition(overLayer, 'absolute');
+    ElementOperator.setBlockDisplay(overLayer);
+    ElementOperator.setPointerEvents(overLayer, 'none');
+    overLayer.style.setProperty('background', 'transparent');
+    overLayer.style.setProperty('border', '2px solid #4b89ff');
+    element.append(overLayer);
+    this.selectedCell = overLayer;
   }
 };
